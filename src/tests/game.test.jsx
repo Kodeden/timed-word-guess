5fe1adcdcb86abcbb3abe8797560713d868c6e46
Manav Misra <manavm@visionwebsoft.com>
2023-03-31 18:23:04 -0500

    test: replace underscores with correct guesses
    Includes :( path.

--- src/tests/game.test.jsx
+++ src/tests/game.test.jsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import Game, { replaceUnderscoresWithCorrectGuess } from "../routes/game";
-import { setup } from "./utils";
 
 const input = {
   word: "test",
@@ -9,6 +8,27 @@ const input = {
 };
 
 const initialUnderscores = input.word.replace(/[a-z]/gi, "_");
+
+test("given an underscored word, it will replace ONLY correct guesses with the letter", () => {
+  const displayedWordWithTs = replaceUnderscoresWithCorrectGuess({
+    displayedWord: initialUnderscores,
+    word2Guess: input.word,
+    guessedLetter: "t",
+  });
+
+  const expected = "t__t";
+
+  expect(displayedWordWithTs).toBe(expected);
+
+  const displayedWordWithTsAfterWrongGuesses =
+    replaceUnderscoresWithCorrectGuess({
+      displayedWord: displayedWordWithTs,
+      word2Guess: input.word,
+      guessedLetter: "z",
+    });
+
+  expect(displayedWordWithTsAfterWrongGuesses).toBe(expected);
+});
 
 it("renders underscores for each letter in the word", () => {
   render(<Game gameSettings={input} />);
