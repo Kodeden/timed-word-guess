63dd7d0d5b773d61fd4abbdfcf93c97f18b05fda
Manav Misra <manavm@visionwebsoft.com>
2023-03-31 12:49:38 -0500

    test: reveal a correct letter whenever typed

--- src/tests/game.test.jsx
+++ src/tests/game.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
-import Game from "../routes/game";
+import Game, { replaceUnderscoresWithCorrectGuess } from "../routes/game";
+import { setup } from "./utils";
 
 const input = {
   word: "hello",
@@ -7,18 +8,31 @@ const input = {
   maxTime: "60",
 };
 
+const initialUnderscores = input.word
+  .split(
+    // ⚠️ MUST put empty string to do the split 💖
+    ""
+  )
+  .map(() => "_")
+  .join("");
+
 it("renders underscores for each letter in the word 2 guess", () => {
   render(<Game gameSettings={input} />);
 
-  expect(
-    screen.getByText(
-      input.word
-        .split(
-          // ⚠️ MUST put empty string to do the split 💖
-          ""
-        )
-        .map(() => "_")
-        .join("")
-    )
-  ).toBeInTheDocument();
+  expect(screen.getByText(initialUnderscores)).toBeInTheDocument();
+});
+
+it("reveals a correct letter whenever typed", async () => {
+  const firstGuessedLetter = "h";
+  const { user } = setup(<Game gameSettings={input} />);
+
+  await user.keyboard(firstGuessedLetter);
+
+  const updatedDisplayedWord = replaceUnderscoresWithCorrectGuess({
+    displayedWord: initialUnderscores,
+    word2Guess: input.word,
+    guessedLetter: firstGuessedLetter,
+  });
+
+  expect(screen.getByText(updatedDisplayedWord)).toBeInTheDocument();
 });
