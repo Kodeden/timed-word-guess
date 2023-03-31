e7e474f281f6f20d330f594449cabb04a57d429e
Manav Misra <manavm@visionwebsoft.com>
2023-03-31 16:51:03 -0500

    Add timer
    Includes fixes to accessibility ♿ issues.

---
+++ src/components/timer.jsx
@@ -0,0 +1,37 @@
+import PropTypes from "prop-types";
+import { useEffect } from "react";
+
+export default function Timer({
+  startingTimeInMinutes,
+  timeRemaining,
+  setTimeRemaining,
+}) {
+  useEffect(() => {
+    if (timeRemaining > 0) {
+      const intervalId = setInterval(() => {
+        setTimeRemaining((prevTime) => prevTime - 1);
+      }, 100);
+
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  }, [timeRemaining, setTimeRemaining]);
+
+  const minutes = Math.floor(timeRemaining / 600);
+  const seconds = Math.floor((timeRemaining % 600) / 10);
+  const tenths = timeRemaining % 10;
+
+  return (
+    <p>
+      {minutes}:{seconds.toString().padStart(2, "0")}.{tenths}
+      ⏱️
+    </p>
+  );
+}
+
+Timer.propTypes = {
+  startingTimeInMinutes: PropTypes.number.isRequired,
+  timeRemaining: PropTypes.number.isRequired,
+  setTimeRemaining: PropTypes.func.isRequired,
+};
