9966894a0c6fa4c945570b6814cc71b6a2070bfe
Manav Misra <manavm@visionwebsoft.com>
2023-03-31 18:31:01 -0500

    test: reveals letters

--- src/tests/game.test.jsx
+++ src/tests/game.test.jsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import Game, { replaceUnderscoresWithCorrectGuess } from "../routes/game";
+import { setup } from "./utils";
 
 const input = {
   word: "test",
@@ -34,4 +35,42 @@ it("renders underscores for each letter in the word", () => {
   render(<Game gameSettings={input} />);
 
   expect(screen.getByText(initialUnderscores)).toBeInTheDocument();
+});
+
+it("reveals the letter when the user guesses correctly", async () => {
+  const firstGuess = "t";
+  const { user } = setup(<Game gameSettings={input} />);
+
+  await user.keyboard(firstGuess);
+
+  const updatedDisplayedWord = replaceUnderscoresWithCorrectGuess({
+    displayedWord: initialUnderscores,
+    word2Guess: input.word,
+    guessedLetter: firstGuess,
+  });
+
+  expect(screen.getByText(updatedDisplayedWord)).toBeInTheDocument();
+
+  const secondGuess = "e";
+  await user.keyboard(secondGuess);
+
+  const updatedDisplayedWord2 = replaceUnderscoresWithCorrectGuess({
+    displayedWord: updatedDisplayedWord,
+    word2Guess: input.word,
+    guessedLetter: secondGuess,
+  });
+
+  expect(screen.getByText(updatedDisplayedWord2)).toBeInTheDocument();
+
+  const badGuess = "z";
+
+  await user.keyboard(badGuess);
+
+  const updatedDisplayedWord3 = replaceUnderscoresWithCorrectGuess({
+    displayedWord: updatedDisplayedWord2,
+    word2Guess: input.word,
+    guessedLetter: badGuess,
+  });
+
+  expect(screen.getByText(updatedDisplayedWord3)).toBeInTheDocument();
 });
