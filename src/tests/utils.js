a95f857a3f2fa4dd738fe1f5bdd3499add1c424e
Manav Misra <manavm@visionwebsoft.com>
2023-04-07 18:06:19 -0500

    feat: :technologist: set 'delay: null'
    This is to allow testing of timers.

--- src/tests/utils.js
+++ src/tests/utils.js
@@ -3,7 +3,10 @@ import userEvent from "@testing-library/user-event";
 
 export function setup(jsx) {
   return {
-    user: userEvent.setup(),
+    user: userEvent.setup(
+      // Make sure that this doesn't break any other tests in the suite.
+      { delay: null }
+    ),
 
     // Think of this like a 'mix-in' of the entries from the returned object.
     ...render(jsx),
